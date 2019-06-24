fe0702b2ed004a0b201d97730607bba27aa74af8
Roshantwanabasu <roshan.twanabasu98@gmail.com>
2019-06-24 13:25:27 +0545

    hello world in ruby

---
+++ hello.rb
@@ -0,0 +1,12 @@
+
+#puts vs print
+puts "hello world"
+puts "prints in newline"
+print "print on same line"
+print "also on the same line"
+puts [1,2,4,nil,nil,5]
+print [1,2,nil,5]
+puts
+
+puts "hello world","welcome to ruby"
+
