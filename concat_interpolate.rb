e136211983dfc887ff9ab6894e67eeaa71343afa
Roshantwanabasu <roshan.twanabasu98@gmail.com>
2019-06-24 13:51:07 +0545

    string interpolation

---
+++ concat_interpolate.rb
@@ -0,0 +1,9 @@
+#concate
+first_name = "Roshan"
+last_name = "Twanabasu"
+puts first_name + " " + last_name
+
+
+#interpolation
+puts "#{first_name} is my first name"
+puts "#{last_name} is my last name"
